1071145c0dd7f9ba117b459ee559c4c45baa7d03
Isaiah Hayes <isaiahhayes@Isaiahs-Mac-mini.local>
2025-07-24 19:36:20 -0700

    practice problems 1 to 36

--- zeroTo100Practice.js
+++ zeroTo100Practice.js
@@ -185,3 +185,101 @@ function returnSum() {
     return sum;
 }
 console.log(returnSum());
+
+// 9. Write a function that prints the first 10 square numbers.
+function squareNumbers() {
+    let counter = 1;
+    for (let i = 1; i <= 10; i++) {
+        let sum = i * i;
+        console.log(counter + " squared = " + sum);
+        counter++;
+    }
+}
+squareNumbers();
+
+// 10. Write a function that prints the first 10 cube numbers.
+function cubeNumbers() {
+    let counter = 1;
+    for (let i = 1; i <= 10; i++) {
+        let sum = i * i * i;
+        console.log(counter + " cubed = " + sum);
+        counter++;
+    }
+}
+cubeNumbers();
+
+// 11. Write a function that checks whether a number is divisible by 3.
+function divideByThree(n) {
+    if (n % 3 === 0) {
+        console.log(n + " is divisible by 3");
+    } else {
+        console.log(n + " is NOT divisible by 3");
+    }
+}
+divideByThree(26);
+
+// 12. Write a function that checks whether a number is divisible by 5.
+function divisibleByFive(n) {
+    if (n % 5 === 0) {
+        console.log(n + " is divisible by 5");
+    } else {
+        console.log(n + " is NOT divisible by 5");
+    }
+}
+divisibleByFive(20);
+
+// 13. Write a function that checks whether a number is divisible by both 3 and 5.
+function isDivisible(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log(n + " is divisible by 3 and 5");
+    } else {
+        console.log(n + " is NOT divisible by 3 and 5");
+    }
+}
+isDivisible(30);
+
+// 14. Write a function that prints "Fizz" for numbers divisible by 3, "Buzz" for numbers divisible by 5, and "FizzBuzz" for both.
+function divisibleFizzBuzz(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (n % 3 === 0) {
+        console.log("Fizz");
+    } else if (n % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log("Number is NOT divisible by 3 or 5")
+    }
+}
+divisibleFizzBuzz(60);
+
+// 15. Write a function that prints even numbers between two values (inclusive).
+function evenNumbers(n, x) {
+    for (let i = n; i <= x; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+evenNumbers(4, 10);
+
+// 16. Write a function that prints odd numbers between two values (inclusive).
+function oddNumbers(n, x) {
+    for (let i = n; i <= x; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}
+oddNumbers(3, 11);
+
+// 17. Write a function that returns the factorial of a number.
+function factorial(n) {
+    totalFactorial = 1;
+    for (let i = n; i >= 1; i--) {
+        totalFactorial = totalFactorial * i; // or totalFactorial *= i;
+    }
+    return totalFactorial;
+}
+console.log(factorial(5));
+
+// 18. Write a function that prints all multiples of 4 from 1 to 100.
